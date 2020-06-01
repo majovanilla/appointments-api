4d3d4c7abbd62a7f250a534a7b40aade24cb0b8c
majovainilla <maria_reyes@me.com>
2020-05-31 21:00:01 -0500

    Add model testing

---
+++ spec/models/tutor_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Tutor, type: :model do
+  it { should have_many(:appointments).dependent(:destroy) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:subject) }
+  it { should validate_presence_of(:about) }
+end
