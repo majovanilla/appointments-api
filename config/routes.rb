cfd46c8d65dbf038218b6f6d116badd83ed5f16c
majovainilla <maria_reyes@me.com>
2020-06-02 16:43:21 -0500

    Delete appointments-tutors nested routes and change tests accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :tutors, only: %i[index show] do
+  resources :tutors, only: %i[index show]
+
+  resources :users, only: %i[index show] do
     resources :appointments
   end
 
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
-  # resources :users, only: [:show] do
-  #   resources :appointments
-  # end
 end
