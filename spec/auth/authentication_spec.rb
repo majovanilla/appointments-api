66b970d2066da9add8223bcc2a9c74b62af58063
majovainilla <maria_reyes@me.com>
2020-06-02 12:30:08 -0500

    Add Users requests tests

--- spec/auth/authentication_spec.rb
+++ spec/auth/authentication_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe AuthenticationController, type: :request do
+RSpec.describe 'Authentication', type: :request do
   describe 'POST /auth/login' do
     let!(:user) { create(:user) }
     let(:headers) { valid_headers.except('Authorization') }
