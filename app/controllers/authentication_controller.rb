0188cd4d10b9519febe27dadf197ba6d7f265342
majovainilla <maria_reyes@me.com>
2020-06-02 12:50:57 -0500

    Edit application controller to close API to unauthorized users except create and authorize

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class AuthenticationController < ApplicationController
+  include Response
+  include ExceptionHandler
+
+  skip_before_action :authorize_request, only: :authenticate
+
   def authenticate
     auth_token =
       AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  skip_before_action :authorize_request, only: :create
+
   def create
     user = User.create!(user_params)
     auth_token = AuthenticateUser.new(user.email, user.password).call
