672e98963535921aea0419253f8cd92ee824ad43
majovainilla <maria_reyes@me.com>
2020-06-06 19:09:53 -0500

    Edit routes to include appointments/new

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[index show]
 
-  resources :appointments, only: %i[index show create update destroy]
+  resources :appointments, only: %i[index show update destroy]
 
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
+  post 'appointments/new', to 'appointments#create'
 end
