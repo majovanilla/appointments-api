7e22f8ee7cd9858169ebab6b09cc3b03ef975e16
majovainilla <maria_reyes@me.com>
2020-06-10 11:26:30 -0500

    Edit typo on db seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-tutor.create(name: 'Juana Pérez',
+Tutor.create(name: 'Juana Pérez',
              email: 'juana@me.com',
              subject: 'History',
              about: 'Next, Dan Bot uses the information collected to predetermine if there is a winning case. He performs swift hidden moves on the available board cells, first with his marker and then with his opponent’s marker. If placing his marker on any available spot gets him a winning move, then the game is over and he wins or he checkmates and automatically wins the game if there was no other possible move for his opponent. But if there isn’t any winning case, he applies the next line of action.',
@@ -16,7 +16,7 @@ tutor.create(name: 'Juana Pérez',
              rate: '$ 154/hr',
              experience: '4 years')
 
-tutor.create(name: 'Alma Ramírez',
+Tutor.create(name: 'Alma Ramírez',
              email: 'alma@me.com',
              subject: 'Math',
              about: 'Next, Dan Bot uses the information collected to predetermine if there is a winning case. He performs swift hidden moves on the available board cells, first with his marker and then with his opponent’s marker. If placing his marker on any available spot gets him a winning move, then the game is over and he wins or he checkmates and automatically wins the game if there was no other possible move for his opponent. But if there isn’t any winning case, he applies the next line of action.',
@@ -24,7 +24,7 @@ tutor.create(name: 'Alma Ramírez',
              rate: '$ 120/hr',
              experience: '9 years')
 
-tutor.create(name: 'Yolanda Aguirre',
+Tutor.create(name: 'Yolanda Aguirre',
              email: 'yola@me.com',
              subject: 'Science',
              about: 'Next, Dan Bot uses the information collected to predetermine if there is a winning case. He performs swift hidden moves on the available board cells, first with his marker and then with his opponent’s marker. If placing his marker on any available spot gets him a winning move, then the game is over and he wins or he checkmates and automatically wins the game if there was no other possible move for his opponent. But if there isn’t any winning case, he applies the next line of action.',
