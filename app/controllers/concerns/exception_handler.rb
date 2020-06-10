a86d86b5c22f286ee836a53505358fec281b1b43
majovainilla <maria_reyes@me.com>
2020-06-10 11:08:25 -0500

    Edit exception handler file to include more detailed messages

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -10,20 +10,28 @@ module ExceptionHandler
   included do
     rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
     rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
-    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
-    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::MissingToken, with: :missing_token
+    rescue_from ExceptionHandler::InvalidToken, with: :invalid_token
 
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
     end
 
     rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
+      json_response({ message: e.message }, :record_invalid)
     end
   end
 
   def four_twenty_two(e)
     json_response({ message: e.message }, :unprocessable_entity)
+  end
+
+  def missing_token(e)
+    json_response({ message: e.message }, :missing_token)
+  end
+
+  def invalid_token(e)
+    json_response({ message: e.message }, :invalid_token)
   end
 
   def unauthorized_request(e)
