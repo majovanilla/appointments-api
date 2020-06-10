6cda6498be9cec02e9a86dba2d0b5e0fd1555347
majovainilla <maria_reyes@me.com>
2020-06-09 20:35:41 -0500

    Edit typo on jwt file

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -5,7 +5,7 @@ require 'jwt'
 class JsonWebToken
   # secret to encode and decode token
   HMAC_SECRET = Rails.application.secrets.secret_key_base
-  r
+
   def self.encode(payload, exp = 24.hours.from_now)
     # set expiry to 24 hours from creation time
     payload[:exp] = exp.to_i
