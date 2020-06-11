f5fa57393677d58e55019ed9dba43c22d5321738
majovainilla <maria_reyes@me.com>
2020-06-11 16:54:03 -0500

    Add skip authorize request when loading home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  include Response
+  include ExceptionHandler
+
+  skip_before_action :authorize_request, only: :index
+
   def index; end
 end
