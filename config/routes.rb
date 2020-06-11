0b8fa1d7e72c8352d6d50ef164b748becc29bc4a
majovainilla <maria_reyes@me.com>
2020-06-11 12:57:23 -0500

    Add home route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   resources :appointments, only: %i[index show update destroy]
 
-  # get '/', to: 'home#index'
+  get '/', to: 'tutors#index'
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
   post 'appointments/new', to: 'appointments#create'
