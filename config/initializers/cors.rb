b735532b75d75bdbc9646a0b82e80b007d249bb3
majovainilla <maria_reyes@me.com>
2020-06-12 20:34:42 -0500

    Edit cors file to delete typo

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,6 +14,5 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head]
-             Access-Control-Allow-Credentials: true
   end
 end
